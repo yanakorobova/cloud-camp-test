587ffe74691b39a322e47eba2ef78f1c3396ddd3
yanakorobova <08mickey01@gmail.com>
2023-06-18 17:54:51 +0300

    ref: cleanup code

--- src/common/validate/validate.ts
+++ src/common/validate/validate.ts
@@ -2,7 +2,7 @@ import * as Yup from 'yup'
 
 export const validateStep1 = Yup.object().shape({
     nickname: Yup.string().max(30).matches(/^[а-яёa-z0-9]+$/i, '\n' +
-        'Can consist of a letter and a number').required('Enter your nickname'),
+        'Can consist of letters and numbers').required('Enter your nickname'),
     name: Yup.string().max(50).matches(/^[а-яёa-z]+$/i, 'Must contain only letters')
         .required('Enter your name'),
     surname: Yup.string().max(50).matches(/^[а-яёa-z]+$/i, 'Must contain only letters')
